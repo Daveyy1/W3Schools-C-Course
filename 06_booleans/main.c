a3651f94d2483a6bfebd627ace648c3145b0b38f
Daveyy1 <daveygamingyt@gmail.com>
2026-03-06 10:30:28 +0100

    finished boolean chapter

--- 06_booleans/main.c
+++ 06_booleans/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    bool valid = true;
+    bool invalid = false;
+    bool isHamburgerTasty = true;
+    bool isPizzaTasty = true;
+
+    printf("%d\n", valid);
+    printf("%d\n", invalid);
+    printf("%d\n", 10 > 9);
+    printf("%d\n", 10 == 10); // Prints 1 (true), because 10 is equal to 10
+    printf("%d\n", 10 == 15); // Prints 0 (false), because 10 is not equal to 15
+    printf("%d\n", 5 == 55);  // Prints 0 (false), because 5 is not equal to 55
+
+    // Find out if both hamburger and pizza are tasty
+    printf("%d\n", isHamburgerTasty == isPizzaTasty);
+
+    int myAge = 25;
+    int votingAge = 18;
+
+    if (myAge >= votingAge) {
+        printf("Old enough to vote!");
+    } else {
+        printf("Not old enough to vote.");
+    }
+
+    // CODING CHALLENGE //
+    // Declare and assign age
+    int age = 15;
+    // Declare and assign minAge
+    int minAge = 12;
+    // Print the result of: age >= minAge
+    printf("%d\n", age >= minAge);
     return 0;
 }
